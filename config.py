2e6df44d6a5511e7bc35bd90f42344f316a19316
GuizaZeineb <45284041+GuizaZeineb@users.noreply.github.com>
2022-05-01 19:33:47 +0200

    Settings with MicrosoftApp

--- config.py
+++ config.py
@@ -10,5 +10,7 @@ class DefaultConfig:
     load_dotenv()
 
     PORT = 3978
-    APP_ID = os.environ.get("MicrosoftAppId", "")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
+    APP_ID = os.environ.get("MicrosoftAppId", "3c699fe5-4776-47a0-93ca-baa0258c7958")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "sjD8Q~pzxGQ3FQHil2TLW9Fg6Gsg.zRW2vA.Ydqs")
+    #SETTINGS = BotFrameworkAdapterSettings("3c699fe5-4776-47a0-93ca-baa0258c7958", "sjD8Q~pzxGQ3FQHil2TLW9Fg6Gsg.zRW2vA.Ydqs")
+
